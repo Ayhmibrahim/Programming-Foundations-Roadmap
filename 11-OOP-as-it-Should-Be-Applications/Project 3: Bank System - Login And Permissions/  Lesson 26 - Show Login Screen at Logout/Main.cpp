2282e8a3446cfc65d40df7f1e5b6c3c9484540f2
Ali Rifai <ammmrrr18@gmail.com>
2026-03-04 16:20:52 +0200

    Create Main.cpp

---
+++ 11-OOP-as-it-Should-Be-Applications/Project 3: Bank System - Login And Permissions/  Lesson 26 - Show Login Screen at Logout/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "clsLoginScreen.h"
+
+
+int main()
+
+{
+
+    while (true)
+    {
+        clsLoginScreen::ShowLoginScreen();
+    }
+
+    system("pause>0");
+    return 0;
+}
